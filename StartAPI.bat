0935a95e4a72f19ca07e5c753a0187c340fa46cb
Tibo <tibodemunck@live.be>
2020-05-29 13:29:17 +0200

    auto installs pm2 -g

--- StartAPI.bat
+++ StartAPI.bat
@@ -1,3 +1,4 @@
 @echo off
 cd /d "%~dp0/nodeJS-server"
-IF exist node_modules ( pm2 start android_remote_api.js ) ELSE ( npm install && pm2 start android_remote_api.js )
+WHERE pm2
+IF %ERRORLEVEL% NEQ 0 (npm install pm2 -g && IF exist node_modules ( pm2 start android_remote_api.js ) ELSE ( npm install && pm2 start android_remote_api.js )) ELSE (IF exist node_modules ( pm2 start android_remote_api.js ) ELSE ( npm install && pm2 start android_remote_api.js ))
